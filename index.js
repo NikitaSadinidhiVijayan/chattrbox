00a39056554842159aa35b7b7c849c04a2106601
NikitaSadinidhiVijayan <sv.nikita91@yahoo.com>
2017-04-19 20:45:05 -0700

    silver challenge
    Added MIME code

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var http = require('http');
 var fs = require('fs');
 var extract = require('./extract');
 var wss = require('./websockets-server');
+var mime=require('mime'); // silver challenge mime type for pdf, img files
 
 var handleError = function(err, res) {
     res.writeHead(404);
@@ -16,8 +17,10 @@ var server = http.createServer(function(req, res) {
             handleError(err, res);
             return;
         } else {
+            res.writeHead(200,{'Content-Type':mime.lookup(filePath)});
             res.end(data);
         }
     });
 });
+//changed the server port inorder to avoid clash with other project
 server.listen(3001);
